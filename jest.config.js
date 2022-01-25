e97f6b9be5d51906f8614e419f3026f2ace0ec73
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-01-25 01:13:50 -0300

    feat: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  testMatch: ['**/*.spec.ts']
+}
