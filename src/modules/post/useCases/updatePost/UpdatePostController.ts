964b7d270d5dc14863472b97210bd3a8e4741739
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-01-28 02:08:50 -0300

    feat: add UpdatePostController

---
+++ src/modules/post/useCases/updatePost/UpdatePostController.ts
@@ -0,0 +1,55 @@
+import { Controller } from '../../../../shared/infra/http/Controller'
+import { badRequest, HttpAuthenticatedRequest, HttpResponse, ok, serverError } from '../../../../shared/infra/http/http'
+import { Validator } from '../../../../validation/Validator'
+import { PresentationUpdatedPost } from '../../models/mapper/PresentationUpdatedPost'
+import { UpdatePost, UpdatePostInputDTO } from './UpdatePost'
+import { UpdatePostError } from './UpdatePostErrors'
+
+export type UpdatePostControllerRequest = HttpAuthenticatedRequest<{
+  title: string
+  content: string
+}, {
+  postId: string
+}>
+
+export class UpdatePostController extends Controller {
+  constructor (
+    private readonly validator: Validator,
+    private readonly updatePost: UpdatePost
+  ) {
+    super()
+  }
+
+  async execute (request: UpdatePostControllerRequest): Promise<HttpResponse<any>> {
+    try {
+      const validationError = this.validator.validate(request.body)
+      if (validationError) return badRequest(validationError)
+
+      const updatePostPayload: UpdatePostInputDTO = {
+        title: request.body!.title,
+        content: request.body!.content,
+        postId: request.params!.postId,
+        userPerformingOperation: request.authenticatedUser
+      }
+
+      const updatedPost = await this.updatePost.execute(updatePostPayload)
+
+      const presenationUpdatedPost:PresentationUpdatedPost = {
+        title: updatedPost.title,
+        content: updatedPost.content,
+        userId: updatedPost.user.id
+      }
+
+      return ok(presenationUpdatedPost)
+    } catch (error) {
+      if (error instanceof Error) {
+        switch (error.constructor) {
+          case UpdatePostError.UnauthorizedToUpdatePostError:
+            return badRequest(error)
+        }
+      }
+
+      return serverError(error)
+    }
+  }
+}
---
+++ src/modules/post/useCases/updatePost/__tests__/UpdatePostController.spec.ts
@@ -0,0 +1,107 @@
+import faker from 'faker'
+import { mock } from 'jest-mock-extended'
+
+import presentationPostFactory from '../../../../../../tests/factories/presentationPostFactory'
+import userFactory from '../../../../../../tests/factories/userFactory'
+import { badRequest, ok, serverError } from '../../../../../shared/infra/http/http'
+import { Validator } from '../../../../../validation/Validator'
+import { UpdatePost } from '../UpdatePost'
+import { UpdatePostController, UpdatePostControllerRequest } from '../UpdatePostController'
+import { UpdatePostError } from '../UpdatePostErrors'
+
+const makeSut = () => {
+  const validatorMock = mock<Validator>()
+  const updatePost = mock<UpdatePost>()
+
+  const updatedPost = presentationPostFactory(1)
+
+  // golden path mock
+  validatorMock.validate.mockReturnValue(null)
+  updatePost.execute.mockResolvedValue(updatedPost)
+
+  const sut = new UpdatePostController(
+    validatorMock,
+    updatePost
+  )
+
+  return {
+    sut,
+    validatorMock,
+    updatePost,
+    updatedPost
+  }
+}
+
+const makeRequest = (): UpdatePostControllerRequest => ({
+  authenticatedUser: userFactory(1),
+  body: {
+    title: faker.random.words(),
+    content: faker.random.words()
+  },
+  params: {
+    postId: faker.datatype.uuid()
+  }
+})
+
+describe('UpdatePostController', () => {
+  let request: UpdatePostControllerRequest
+
+  beforeEach(() => {
+    request = makeRequest()
+  })
+
+  it('should call Validator with correct values', async () => {
+    const { sut, validatorMock } = makeSut()
+    await sut.execute(request)
+    expect(validatorMock.validate).toHaveBeenCalledWith(request.body)
+  })
+
+  it('should return a badRequest with validation error if Validator returns an error', async () => {
+    const { sut, validatorMock } = makeSut()
+    validatorMock.validate.mockReturnValueOnce(new Error())
+    const response = await sut.execute(request)
+    expect(response).toEqual(badRequest(new Error()))
+  })
+
+  it('should return a serverError if Validator throws', async () => {
+    const { sut, validatorMock } = makeSut()
+    validatorMock.validate.mockImplementationOnce(() => { throw new Error() })
+    const response = await sut.execute(request)
+    expect(response).toEqual(serverError(new Error()))
+  })
+
+  it('should call UpdatePost with correct values', async () => {
+    const { sut, updatePost } = makeSut()
+    await sut.execute(request)
+    expect(updatePost.execute).toHaveBeenCalledWith({
+      title: request.body?.title,
+      content: request.body?.content,
+      postId: request.params!.postId,
+      userPerformingOperation: request.authenticatedUser
+    })
+  })
+
+  it('should a badRequest with UnauthorizedToUpdatePostError if GetPost throws a UnauthorizedToUpdatePostError', async () => {
+    const { sut, updatePost } = makeSut()
+    updatePost.execute.mockRejectedValueOnce(new UpdatePostError.UnauthorizedToUpdatePostError())
+    const response = await sut.execute(request)
+    expect(response).toEqual(badRequest(new UpdatePostError.UnauthorizedToUpdatePostError()))
+  })
+
+  it('should a serverError if GetPost throws a unkown error', async () => {
+    const { sut, updatePost } = makeSut()
+    updatePost.execute.mockRejectedValueOnce(new Error())
+    const response = await sut.execute(request)
+    expect(response).toEqual(serverError(new Error()))
+  })
+
+  it('should return ok with with PresentationUpdatedPost', async () => {
+    const { sut, updatedPost } = makeSut()
+    const response = await sut.execute(request)
+    expect(response).toEqual(ok({
+      title: updatedPost.title,
+      content: updatedPost.content,
+      userId: updatedPost.user.id
+    }))
+  })
+})
