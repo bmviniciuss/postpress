d02e90082f0a7cfc29fb69a0e363b11654d0ea6e
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-01-27 22:42:27 -0300

    test: add GET /posts integration tests

--- src/shared/infra/http/routes/__tests__/post.test.ts
+++ src/shared/infra/http/routes/__tests__/post.test.ts
@@ -1,6 +1,8 @@
 import { PrismaClient, User } from '@prisma/client'
+import { omit } from 'lodash'
 import request from 'supertest'
 
+import postFactory from '../../../../../../tests/factories/postFactory'
 import userFactory from '../../../../../../tests/factories/userFactory'
 import { buildAuthUser } from '../../../../../../tests/utils/buildAuthUser'
 import { makeBearerToken } from '../../../../../../tests/utils/makeBearerToken'
@@ -92,6 +94,62 @@ describe('Post Routes', () => {
       expect(response.body.title).toEqual('title')
       expect(response.body.content).toEqual('content')
       expect(response.body.userId).toEqual(authUser.id)
+    })
+  })
+
+  describe('GET /post', () => {
+    it('should return 401 if authentication is not provided', async () => {
+      const response = await request(app)
+        .get('/post')
+        .send()
+      expect(response.statusCode).toEqual(401)
+      expect(response.body).toMatchInlineSnapshot(`
+        Object {
+          "message": "Token não encontrado",
+        }
+      `)
+    })
+
+    it('should return 401 if an invalid authentication is provided', async () => {
+      const response = await request(app)
+        .get('/post')
+        .set('authorization', 'Bearer random')
+        .send()
+      expect(response.statusCode).toEqual(401)
+      expect(response.body).toMatchInlineSnapshot(`
+        Object {
+          "message": "Token expirado ou inválido",
+        }
+      `)
+    })
+
+    it('should 200 on success', async () => {
+      await prisma.post.create({
+        data: {
+          ...omit(postFactory(1), 'userId'),
+          user: {
+            connect: {
+              id: authUser.id
+            }
+          }
+        }
+      })
+      await prisma.post.create({
+        data: {
+          ...omit(postFactory(1), 'userId'),
+          user: {
+            connect: {
+              id: authUser.id
+            }
+          }
+        }
+      })
+      const response = await request(app)
+        .get('/post')
+        .set('authorization', makeBearerToken(authUser.accessToken!))
+        .send({ title: 'title', content: 'content' })
+      expect(response.statusCode).toEqual(200)
+      expect(response.body.length).toEqual(2)
     })
   })
 })
