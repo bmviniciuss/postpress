17b5dd120c0007552f1882808833d3c08ea96229
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-01-30 03:16:51 -0300

    fix: ambientes com docker

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const PORT = Number(process.env.PORT) || 4000
+export const PORT = 4000
 export const JWT_SECRET = process.env.JWT_SECRET || 'Super Secure JWT Secret'
 export const SALT_NUMBER = 10
